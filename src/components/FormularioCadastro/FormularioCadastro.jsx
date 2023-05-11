0bc0dac7e66c0bd554e4499b08b72eb58cf66333
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-05-11 12:10:23 -0300

    controle de formulário

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,9 +3,18 @@ import Button from "@material-ui/core/Button";
 import { TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
+  let nome = "";
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(nome);
+      }}
+    >
       <TextField
+        onChange={(event) => {
+          nome = event.target.value;
+        }}
         id="nome"
         label="Nome"
         variant="outlined"
@@ -26,13 +35,15 @@ function FormularioCadastro() {
         margin="normal"
         fullWidth
       />
-      <FormControlLabel 
-        label="Promoções" control=
-        {<Switch name="promocoes" defaultChecked color="primary" />}/>
-      
-      <FormControlLabel 
-        label="Novidades" control=
-        {<Switch name="novidades" defaultChecked color="primary" />}/>
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promocoes" defaultChecked color="primary" />}
+      />
+
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultChecked color="primary" />}
+      />
 
       <Button type="submit" variant="contained" color="primary">
         Cadastrar
